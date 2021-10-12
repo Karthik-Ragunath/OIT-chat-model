93c743a6baa11c89d7fc6d24862b29c5a4cae640
Ubuntu <ubuntu@ip-172-31-87-206.ec2.internal>
2021-10-12 23:41:42 +0000

    WIP - adding queues when receiver not yet configured

--- client_1.py
+++ client_1.py
@@ -2,17 +2,33 @@ import websockets
 import asyncio
 import json
 
-def create_message_content():
-    msg_dict = {"from":"client_1", "to":"client_2"}
+def create_dm_message_content():
+    msg_dict = {"from_id":"1", "to_id":"2", "message":"Hello there!!! Hope you are doing?!!!"}
+    msg_dict_dump = json.dumps(msg_dict)
+    return msg_dict_dump
+
+def register_message_content():
+    msg_dict = {"register": True}
+    msg_dict_dump = json.dumps(msg_dict)
+    return msg_dict_dump
+
+def create_group_message():
+    msg_dict = {"from_id":"1", "group_message":"Hello there!!! lets talk right?"}
     msg_dict_dump = json.dumps(msg_dict)
     return msg_dict_dump
 
 async def listen():
     url = "ws://127.0.0.1:7890"
     async with websockets.connect(url) as ws:
-        message_content = create_message_content()
+        register_content = register_message_content()
+        await ws.send(register_content)
+        msg = await ws.recv()
+        print(msg)
+
+        message_content = create_dm_message_content()
         await ws.send(message_content)
         while True:
             msg = await ws.recv()
             print(msg)
+
 asyncio.get_event_loop().run_until_complete(listen())
--- server.py
+++ server.py
@@ -1,23 +1,99 @@
 import websockets
 import asyncio
+import json
+from redis import Redis
 
 PORT_TO_LISTEN = 7890
 
 print("Server listening on port:", PORT_TO_LISTEN)
 
-connected = set()
+connected_set = set()
+connection_list = []
+r = Redis()
+
+class connection_object(object):
+    def __init__(self, conn_id, websocket_conn):
+        self.conn_id = conn_id
+        self.websocket_conn = websocket_conn
+
+    def get_object(self):
+        return self.websocket_conn
+
+def extract_info(message):
+    message_parser = dict()
+    message_parser['is_valid'] = False
+
+    if message.get("register", None):
+        message_parser['register'] = True
+        message_parser['is_valid'] = True
+        return message_parser
+
+    message_parser['from_id'] = message['from_id']
+    if not message.get('message', None):
+        # No message param in message dictionary, nothing to do here
+        return message_parser
+
+    if message.get('is_group', None):
+        message_parser['is_group'] = True
+        message_parser['is_valid'] = True
+        return message_parser
+    else:
+        message_parser['is_group'] = False
+    if not message_parser['is_group']:
+        if message.get('to_id', None):
+            message_parser['to_id'] = message['to_id']
+            message_parser['is_valid'] = True
+        else:
+            message_parser['is_valid'] = False
+    return message_parser
 
 async def echo(websocket, path):
     print("A client just connected")
-    connected.add(websocket)
     try:
         async for message in websocket:
+            try:
+                deserialized_message = json.loads(message)
+                print("Message Log:", deserialized_message, "Type:", type(deserialized_message))
+            except Exception as e:
+                print("Message is not in a state where it could be deserialize the messgae. Skipping this message.")
+                continue
+            message_info = extract_info(deserialized_message)
+            if not message_info.get('is_valid', False):
+                print("Invalid message, nothing to do here")
+                continue
+            # Add Authentication Here
+            if websocket not in connected_set:
+                if message_info.get('register', False):
+                    connected_set.add(websocket)
+                    conn_id = len(connection_list) + 1
+                    connection_list.append(connection_object(conn_id, websocket))
+                    conn_obj = connection_list[-1]
+                    await conn_obj.websocket_conn.send("Device Successfully Registered")
+                else:
+                    print("Must register the device first")
+                print("Came Here.......")
+                continue
+            else:
+                from_id = int(message_info['from_id']) - 1
+                from_conn_obj = connection_list[from_id]
             print("Received message from client: " + message)
-            ## for broadcasting to everyone
+            ## for broadcasting to everyone; basically message from server
             #await websocket.send("Pong: " + "Thanks for the message. I will do the needful")
-            for conn in connected:
-                if conn != websocket:
-                    await conn.send("Someone sent a  message to server")
+            if message_info.get('is_group', False):
+                for conn in connected:
+                    if conn != conn_obj:
+                        await conn.send("Message from " + message_info['from_id'] + ": " + message)
+            else:
+                to_id = int(message_info['to_id']) - 1
+                to_conn_object = connection_list[to_id]
+                try:
+                    to_conn_object.send("Message from " + message_info["to_id"] + ": " + message)
+                except Exception as e:
+                    dm_dict = dict()
+                    dm_dict['from_id'] = str(from_id + 1)
+                    dm_dict['to_id'] = str(to_id + 1)
+                    r_queue.push("dm_messages", json.dumps(dm_dict))
+                    print("dm message pushed to wait queue")
     except websockets.exceptions.ConnectionClosed as e:
         print("A client just disconnected")
 
