4feb870528e3ae2c77aeb000efce106a65289439
Karthik Ragunath Ananda Kumar <a.karthikragunath@gmail.com>
2021-10-15 10:41:18 -0500

    adding authentication infra

--- server.py
+++ server.py
@@ -9,60 +9,95 @@ PORT_TO_LISTEN = 7890
 print("Server listening on port:", PORT_TO_LISTEN)
 
 connected_set = set()
-connection_list = []
 r_queue = Redis()
 redis_host = "auth-data.44nnpy.ng.0001.use1.cache.amazonaws.com"
 
 r_auth_checker = Redis(host=redis_host, port=6379)
+connection_dict = dict()
 
 
 class connection_object(object):
-    def __init__(self, conn_id, websocket_conn):
-        self.conn_id = conn_id
+    def __init__(self, device_name, websocket_conn):
+        self.device_name = device_name
         self.websocket_conn = websocket_conn
 
     def get_object(self):
         return self.websocket_conn
 
+
 def generate_hash(hash_len=20):
     auth = ''.join(random.choices(string.ascii_letters + string.digits, k=hash_len))
     return auth
 
-def set_auth_token_hash():
-    auth = generate_hash(hash_len=16)
+
+def get_device_mappings(auth_key, websocket, register=False):
+    auth_hash = None
+    device_mapping = None
+    session_table_update = False
+    if auth_key and r_auth_checker.hexists('auth_hash', auth_key):
+        auth_hash = str(r_auth_checker.hget("auth_hash", auth_key))
+    if auth_hash and r_auth_checker.hexists('device_mapping', auth_hash):
+        device_mapping = str(r_auth_checker.hget("device_mapping", auth_hash))
+    if register:
+        session_table_update = r_auth_checker.hset("session_table", device_mapping_string, websocket)    
+        if auth_hash and device_mapping and session_table_update:
+            return auth_hash, device_mapping, session_table_update
+    else:
+        if auth_hash and device_mapping:
+            return auth_hash, device_mapping, None
+    return None, None, None
+
+
+def get_reverse_device_mapping(device_mapping):
+    if r_auth_checker.hexists('reverse_device_mapping', device_mapping):
+        return str(r_auth_checker.hget('reverse_device_mapping', device_mapping))
+    else:
+        return None
+
+
+def set_auth_token_hash(device_mapping):
+    auth_key = generate_hash(hash_len=16)
     auth_hash = generate_hash(hash_len=32)
-    auth_tuple = (auth, auth_hash)
+    auth_tuple = (auth_key, auth_hash)
+    r_auth_checker.hset('auth_hash', auth_key, auth_hash)
+    r_auth_checker.hset('device_mapping', auth_hash, device_mapping)
+    r_auth_checker.hset('reverse_device_mapping', device_mapping, auth_hash)
     return auth_tuple
 
-def authentication(auth_token):
-    r_auth_token = r_auth_checker.hexists("auth_token", auth_token)
-    if r_auth_token:
-        message_token_hash = r_auth_checker.hget("auth_token", auth_token)
-        return message_token_hash
-    else:
-        return False
 
-
-def extract_info(message):
+def extract_info(message, websocket):
     message_parser = dict()
     message_parser['is_valid'] = False
+    auth_key = message.get('auth_key', None)
+    auth_hash = None
+    device_mapping = None
 
-    if message.get("register", None):
-        message_parser['register'] = True
-        message_parser['device_name'] = message['device_name']
-        message_parser['is_valid'] = True
-        auth_tuple = set_auth_token_hash()
-        if auth_tuple:
-            message_parser['auth_key'] = auth_tuple[0]
-            message_parser['auth_hash'] = auth_tuple[1]
+    if auth_key:
+        auth_hash, device_mapping, _ = get_device_mappings(auth_key)
+        if auth_hash, device_mapping:
+            message_parser['auth_key'] = auth_key
+            message_parser['auth_hash'] = auth_hash
+            message_parser['device_mapping'] = device_mapping
+
+    elif message.get("register", None):
+        if not auth_key:
+            message_parser['register'] = True
+            message_parser['device_mapping'] = message['device_name']
+            auth_tuple = set_auth_token_hash()
+            if auth_tuple and auth_tuple[0] and auth_tuple[1] and message_parser['device_mapping']:
+                auth_hash, device_mapping, session_table_update = get_device_mappings(auth_tuple[0], websocket, register=True)
+                if auth_hash:
+                    message_parser['auth_key'] = auth_tuple[0]
+                    message_parser['auth_hash'] = auth_hash
+                    message_parser['device_mapping'] = device_mapping
+                    connection_dict[device_mapping] = websocket
+                    message_parser['is_valid'] = True
         return message_parser
 
-    auth_key = messgae_parser.get('auth_key', None)
-    auth_token = authentication(auth_key)
-    if not auth_token:
-        return message_parser
+    else:
+        print("Cannot authenticate")
 
-    message_parser['from_id'] = message['from_id']
+    message_parser['from_id'] = message_parser['device_mapping']
     if not message.get('message', None):
         # No message param in message dictionary, nothing to do here
         return message_parser
@@ -91,15 +126,13 @@ async def check_dm_queue():
                 message = r_queue.lpop("dm_messages")
                 if message:
                     deserialized_message = json.loads(message)
-                    from_id = int(deserialized_message['from_id'])
-                    to_id = int(deserialized_message['to_id'])
+                    from_id = deserialized_message['from_id']
+                    to_id = deserialized_message['to_id']
                     dm_message = deserialized_message['message']
-                    # Temporary Hack
-                    if len(connection_list) >= from_id and len(connection_list) >= to_id:
-                        from_conn_object = connection_list[from_id - 1]
-                        to_conn_object = connection_list[to_id - 1]
-                        from_str = str(from_id - 1)
-                        await to_conn_object.websocket_conn.send("Message from " + from_str + ": " + dm_message)
+                    if from_id in connection_dict and to_id in connection_dict:
+                        from_conn_object = connection_dict[from_id]
+                        to_conn_object = connection_dict[to_id]
+                        await to_conn_object.websocket_conn.send("Message from " + from_id + ": " + dm_message)
                         print("One less message in dm queue, pheww")
                     else:
                         r_queue.lpush("dm_messages", message)
@@ -125,20 +158,19 @@ async def echo(websocket, path):
             # Add Authentication Here
             if websocket not in connected_set:
                 if message_info.get('register', False):
-                    connected_set.add(websocket)
-                    conn_id = len(connection_list) + 1
-                    connection_list.append(connection_object(conn_id, websocket))
-                    conn_obj = connection_list[-1]
-                    await conn_obj.websocket_conn.send("Device Successfully Registered")
-                    r_auth_checker.hset("auth_token", message_info[0], message_info[1])
-                    r_auth_checker.hset("device_mapping", message_info['device_name'], mesage_info['auth_hash'])
-                    r_session = r_auth_checker.hset("session_table", message_info['device_name'], websokcet)
+                    if message_info.get('is_valid', False):
+                        connected_set.add(websocket)
+                        conn_obj = connection_object(conn_id, websocket)
+                        connection_dict[message_info['device_mapping']] = conn_obj
+                        await conn_obj.websocket_conn.send("Device Successfully Registered")
+                    else:
+                        await conn_obj.websocket_conn.send("Could Not Register Your Device Successfully")
 :                else:
-                    print("Must register the device first")
+                     await conn_obj.websocket_conn.send("Must Register Your Device First")
                 continue
             else:
-                from_id = int(message_info['from_id']) - 1
-                from_conn_obj = connection_list[from_id]
+                from_id = message_info['device_name']
+                from_conn_obj = connection_object[from_id]
             print("Received message from client: " + message)
             ## for broadcasting to everyone; basically message from server
             #await websocket.send("Pong: " + "Thanks for the message. I will do the needful")
@@ -146,21 +178,25 @@ async def echo(websocket, path):
                 for conn in connected:
                     if conn != from_conn_obj:
                         await conn.send("Message from " + message_info['from_id'] + ": " + message)
+                print("group message sent successfully")
             else:
                 try:
-                    to_id = int(message_info['to_id']) - 1
-                    to_conn_object = connection_list[to_id]
-                    await to_conn_object.websocket_conn.send("Message from " + message_info["to_id"] + ": " + message_info['message'])
+                    to_id = message_info['to_id']
+                    to_conn_object = connection_dict[to_id]
+                    await to_conn_object.websocket_conn.send("Message from " + from_id + ": " + message_info['message'])
+                    print("dm message sent successfully")
                 except Exception as e:
                     print("Exception is:", e)
                     dm_dict = dict()
-                    dm_dict['from_id'] = str(from_id + 1)
-                    dm_dict['to_id'] = str(to_id + 1)
+                    dm_dict['from_id'] = from_id
+                    dm_dict['to_id'] = to_id
                     dm_dict['message'] = message_info['message']
                     r_queue.lpush("dm_messages", json.dumps(dm_dict))
                     print("dm message pushed to wait queue")
+                    await to_conn_object.websocket_conn.send(to_id + " is not online yet")
     except websockets.exceptions.ConnectionClosed as e:
         print("A client just disconnected")
+
 
 start_server = websockets.serve(echo, "0.0.0.0", PORT_TO_LISTEN, reuse_port=True)
 asyncio.get_event_loop().run_until_complete(start_server)
